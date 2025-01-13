5f510877b8e6cfe9ba7a9ea0bab2a5210f6f9dbe
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-13 17:19:15 -0300

    criado model do produto e melhorado design
    criado modelo para cadastro de produtos
    
    melhorado layout do mainLayout para que os links abram em uma segunda
    aba
    
    continuado o layout da pagina inicial (ainda em desenvolvimento)

---
+++ ProautoWebApp/Models/Product.cs
@@ -0,0 +1,15 @@
+namespace ProautoWebApp.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ProductName { get; set; }
+    public string Color { get; set; }
+    public string Dilution { get; set; }
+    public double Packaging { get; set; }
+    public int Quantity { get; set; }
+    public string Image { get; set; }
+    public string Department { get; set; }
+}
