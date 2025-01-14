0b81c73a4110aae3cb6d7b8b30f1625c787a7234
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-14 13:36:41 -0300

    melhoria do model conforme solicitado pela diretoria

--- ProautoWebApp/Models/Product.cs
+++ ProautoWebApp/Models/Product.cs
@@ -8,8 +8,8 @@ public class Product
     public string ProductName { get; set; }
     public string Color { get; set; }
     public string Dilution { get; set; }
-    public double Packaging { get; set; }
+    public string Packaging { get; set; }
     public int Quantity { get; set; }
     public string Image { get; set; }
-    public string Department { get; set; }
+    public string Category { get; set; }
 }
