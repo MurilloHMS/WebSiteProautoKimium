3a0b41916c2cbcba2001031fb98a83acadb2949f
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-29 11:19:00 -0300

    criado tela de contato
    finalizado layout da tela de contato e criado classe para armazenar os dados

---
+++ ProautoWebApp/Models/Contato.cs
@@ -0,0 +1,11 @@
+namespace ProautoWebApp.Models;
+
+public class Contatos
+{
+    public int ContatoId { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public string NomeEmpresa { get; set; }
+    public string Mensagem { get; set; }
+}
